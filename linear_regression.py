85f07e9a9a6ea499749830ddc9f6fa434cb21e92
Bineesh Chandrasekharan <bineeshpc@gmail.com>
2023-12-12 20:28:12 +0530

    base model

--- linear_regression.py
+++ linear_regression.py
@@ -39,6 +39,7 @@ import sklearn.preprocessing as preprocessing
 import sklearn.model_selection as model_selection
 import sklearn.feature_selection as feature_selection
 import sklearn.pipeline as pipeline
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.feature_selection import RFE
@@ -70,12 +71,17 @@ print("number of columns: ", num_columns)
 #%%
 # check the data types of the columns
 df.info()
+
+#%% [markdown]
+
+# ## Data Cleaning
 #%%
 # identify the columns with null values
 null_value_columns = list(df.columns[df.isnull().sum() > 0])
 
 print("there are {} columns with null values".format(len(null_value_columns)))
 print("columns with null values: ", null_value_columns)
+
 
 #%%
 
@@ -87,12 +93,32 @@ df[null_value_columns].isnull().sum()
 df[null_value_columns].isnull().sum() / num_rows * 100
 
 #%%
+df[null_value_columns].describe()
+
+#%%
+null_values_numerical_columns = df[null_value_columns].describe().columns
+null_values_categorical_columns = list(set(null_value_columns) - set(null_values_numerical_columns))
+
+df[null_values_numerical_columns].describe()
+
+#%%
+
+# replace null values with median values
+
+for null_values_numerical_column in null_values_numerical_columns:
+    median = df[null_values_numerical_column].quantile([.5]).iloc[0]
+    print("Replacing null values of {} with median {}".format(null_values_numerical_column, median))
+    df[null_values_numerical_column].fillna(median, inplace=True)
+
+
+#%%
 # number of numerical variables
 num_numerical_variables = len(df.describe().columns)
 numerical_variables = list(df.describe().columns)   
 
 print("number of numerical variables: ", num_numerical_variables)
 print("numerical variables: ", numerical_variables)
+
 
 #%%
 # find categorical variables
@@ -129,18 +155,92 @@ for categorical_variable in categorical_variables:
 for numerical_variable in numerical_variables:
     sns.distplot(df[numerical_variable])
     plt.show()
-    
+
+
+#%% 
+
+for null_values_categorical_column in null_values_categorical_columns:
+    mode = df[null_values_categorical_column].mode()[0]
+    print("mode of {}: {}".format(null_values_categorical_column, mode))
+    display(df[null_values_categorical_column].value_counts())
+    display(df[null_values_categorical_column].isnull().sum())
+    replacement_value = "None"
+    print("replacing null values of {} with {}".format(null_values_categorical_column, replacement_value))
+    df[null_values_categorical_column].fillna(replacement_value, inplace=True)
+#%% [markdown]
+
+# ## Data Preparation
+
 #%%
 
 df1 = df.drop(columns=['Id'])
 
+
 #%%
 
-# X = df1.drop(columns=['SalePrice'])
-# y = df1['SalePrice']
+# convert categorical variables to categorical type
+for categorical_variable in categorical_variables:
+    df1[categorical_variable] = pd.Categorical(df1[categorical_variable])
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+#%%
 
-# model = Lasso(alpha=0.1)
-# model.fit(X_train, y_train)
-# model.score(X_test, y_test)
+# convert categorical variables to label encoding
+
+label_encoder = LabelEncoder()
+for categorical_variable in categorical_variables:
+    df1[categorical_variable] = label_encoder.fit_transform(df1[categorical_variable])
+
+#%%
+
+def get_metrics(y_train, y_train_pred, y_test, y_test_pred):
+    training_rmse = np.sqrt(metrics.mean_squared_error(y_train, y_train_pred))
+    training_r2 = metrics.r2_score(y_train, y_train_pred)
+    test_rmse = np.sqrt(metrics.mean_squared_error(y_test, y_test_pred))
+    test_r2 = metrics.r2_score(y_test, y_test_pred)
+    training_rss = np.sum(np.square(y_train - y_train_pred))
+    test_rss = np.sum(np.square(y_test - y_test_pred))
+    
+    d = {"r2": [training_r2, test_r2], "rmse": [training_rmse, test_rmse], "rss": [training_rss, test_rss]}
+    
+    return pd.DataFrame(d, index=["training", "test"])
+    
+    
+#%%
+
+X = df1.drop(columns=['SalePrice'])
+y = df1['SalePrice']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create scaler object
+scaler_X = MinMaxScaler()
+scaler_y = MinMaxScaler()
+# Scale the features
+X_train_scaled = scaler_X.fit_transform(X_train)
+X_test_scaled = scaler_X.transform(X_test)
+
+y_train_scaled = scaler_y.fit_transform(y_train.values.reshape(-1, 1))
+y_test_scaled = scaler_y.transform(y_test.values.reshape(-1, 1))
+
+model = Lasso(alpha=0.1)
+
+def train_model(X_train, y_train, X_test, y_test, model):
+
+    model.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+    
+    return model, get_metrics(y_train, y_train_pred, y_test, y_test_pred)
+
+
+model, metrics_df = train_model(X_train_scaled, y_train, X_test_scaled, y_test, model)
+
+metrics_df
+
+#%%
+
+df['SalePrice'].describe()
+#%%
+
+
+pd.Series(model.coef_, X_train.columns).sort_values(ascending=False)
